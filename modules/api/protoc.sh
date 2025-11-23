483ae8252c88d95a502845300c9cfaaae4133ff1
Manuel Bluhm <manuel@ssrc.tii.ae>
2025-11-23 15:44:26 +0400

    feat(agent): add user notification module
    Add user desktop notification module to agent. This module runs a grpc
    service that listens for notifications and writes the messages into a
    socket. A systemd user socket activated unit sends the notification to
    any logged in user with a graphical session.
    
    This pattern separates responsibilities and avoids setuid in the agent.
    
    Signed-off-by: Manuel Bluhm <manuel@ssrc.tii.ae>

--- modules/api/protoc.sh
+++ modules/api/protoc.sh
@@ -12,7 +12,7 @@ gen_protoc() {
 }
 
 if [ $# -eq 0 ]; then
-    set -- admin systemd socket stats hwid locale wifi event
+    set -- admin systemd socket stats hwid locale wifi event notify
 fi
 
 for protodir in "$@"; do
